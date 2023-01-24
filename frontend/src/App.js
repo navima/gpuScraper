bf7dc6182f510439b0518521c1b1e9698641b906
navima <draco.109@gmail.com>
2023-01-24 21:14:15 +0100

    Change indentation to tabs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,30 +6,30 @@ import initSqlJs from "sql.js";
 import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm";
 
 export default function App() {
-  const [db, setDb] = useState(null);
-  const [error, setError] = useState(null);
+	const [db, setDb] = useState(null);
+	const [error, setError] = useState(null);
 
-  useEffect(async () => {
-    // sql.js needs to fetch its wasm file, so we cannot immediately instantiate the database
-    // without any configuration, initSqlJs will fetch the wasm files directly from the same path as the js
-    // see ../craco.config.js
-    try {
-      const response = await fetch("https://raw.githubusercontent.com/navima/gpuScraper/archive/archive.db", {
-        withCredentials: false,
-        crossorigin: true,
-        mode: 'cors'
-      });
-      const buf = await response.arrayBuffer();
-      const SQL = await initSqlJs({ locateFile: () => sqlWasm });
-      setDb(new SQL.Database(new Uint8Array(buf)));
-    } catch (err) {
-      setError(err);
-    }
-  }, []);
+	useEffect(async () => {
+		// sql.js needs to fetch its wasm file, so we cannot immediately instantiate the database
+		// without any configuration, initSqlJs will fetch the wasm files directly from the same path as the js
+		// see ../craco.config.js
+		try {
+			const response = await fetch("https://raw.githubusercontent.com/navima/gpuScraper/archive/archive.db", {
+				withCredentials: false,
+				crossorigin: true,
+				mode: 'cors'
+			});
+			const buf = await response.arrayBuffer();
+			const SQL = await initSqlJs({ locateFile: () => sqlWasm });
+			setDb(new SQL.Database(new Uint8Array(buf)));
+		} catch (err) {
+			setError(err);
+		}
+	}, []);
 
-  if (error) return <pre>{error.toString()}</pre>;
-  else if (!db) return <pre>Loading...</pre>;
-  else return <SQLRepl db={db} />;
+	if (error) return <pre>{error.toString()}</pre>;
+	else if (!db) return <pre>Loading...</pre>;
+	else return <SQLRepl db={db} />;
 }
 
 /**
@@ -37,43 +37,43 @@ export default function App() {
  * @param {{db: import("sql.js").Database}} props
  */
 function SQLRepl({ db }) {
-  const [error, setError] = useState(null);
-  const [results, setResults] = useState([]);
+	const [error, setError] = useState(null);
+	const [results, setResults] = useState([]);
 
-  function exec(sql) {
-    try {
-      // The sql is executed synchronously on the UI thread.
-      // You may want to use a web worker here instead
-      setResults(db.exec(sql)); // an array of objects is returned
-      setError(null);
-    } catch (err) {
-      // exec throws an error when the SQL statement is invalid
-      setError(err);
-      setResults([]);
-    }
-  }
+	function exec(sql) {
+		try {
+			// The sql is executed synchronously on the UI thread.
+			// You may want to use a web worker here instead
+			setResults(db.exec(sql)); // an array of objects is returned
+			setError(null);
+		} catch (err) {
+			// exec throws an error when the SQL statement is invalid
+			setError(err);
+			setResults([]);
+		}
+	}
 
-  return (
-    <div className="App">
-      <h1>React SQL interpreter</h1>
+	return (
+		<div className="App">
+			<h1>React SQL interpreter</h1>
 
-      <textarea
-        onChange={(e) => exec(e.target.value)}
-        placeholder="Enter some SQL. No inspiration ? Try “select sqlite_version()”"
-      ></textarea>
+			<textarea
+				onChange={(e) => exec(e.target.value)}
+				placeholder="Enter some SQL. No inspiration ? Try “select sqlite_version()”"
+			></textarea>
 
-      <pre className="error">{(error || "").toString()}</pre>
+			<pre className="error">{(error || "").toString()}</pre>
 
-      <pre>
-        {
-          // results contains one object per select statement in the query
-          results.map(({ columns, values }, i) => (
-            <ResultsTable key={i} columns={columns} values={values} />
-          ))
-        }
-      </pre>
-    </div>
-  );
+			<pre>
+				{
+					// results contains one object per select statement in the query
+					results.map(({ columns, values }, i) => (
+						<ResultsTable key={i} columns={columns} values={values} />
+					))
+				}
+			</pre>
+		</div>
+	);
 }
 
 /**
@@ -81,28 +81,28 @@ function SQLRepl({ db }) {
  * @param {import("sql.js").QueryExecResult} props
  */
 function ResultsTable({ columns, values }) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          {columns.map((columnName, i) => (
-            <td key={i}>{columnName}</td>
-          ))}
-        </tr>
-      </thead>
+	return (
+		<table>
+			<thead>
+				<tr>
+					{columns.map((columnName, i) => (
+						<td key={i}>{columnName}</td>
+					))}
+				</tr>
+			</thead>
 
-      <tbody>
-        {
-          // values is an array of arrays representing the results of the query
-          values.map((row, i) => (
-            <tr key={i}>
-              {row.map((value, i) => (
-                <td key={i}>{value}</td>
-              ))}
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
-  );
+			<tbody>
+				{
+					// values is an array of arrays representing the results of the query
+					values.map((row, i) => (
+						<tr key={i}>
+							{row.map((value, i) => (
+								<td key={i}>{value}</td>
+							))}
+						</tr>
+					))
+				}
+			</tbody>
+		</table>
+	);
 }
